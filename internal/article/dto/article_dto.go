5e19598e85e794ce29b688d7847199f6ca299459
Saman Nesari <saman@Samans-MBP.lan>
2022-11-24 15:22:33 +0330

    fix: remove dto json method

--- internal/article/dto/article_dto.go
+++ internal/article/dto/article_dto.go
@@ -1,9 +1,6 @@
 package article_dto
 
 import (
-	"encoding/json"
-	"io"
-
 	validator "github.com/go-ozzo/ozzo-validation"
 )
 
@@ -11,16 +8,6 @@ import (
 type CreateArticle struct {
 	Name        string `json:"name"`
 	Description string `json:"desc"`
-}
-
-// FromJSONCreateArticleRequest converts json body request to a CreateArticleRequest struct
-func FromJSONCreateArticleRequest(body io.Reader) (*CreateArticle, error) {
-	createArticleRequest := CreateArticle{}
-	if err := json.NewDecoder(body).Decode(&createArticleRequest); err != nil {
-		return nil, err
-	}
-
-	return &createArticleRequest, nil
 }
 
 func (ca *CreateArticle) ValidateCreateArticleDto() error {
