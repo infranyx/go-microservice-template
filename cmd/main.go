8094bf26363720a391afa70cb43e9a3ae5158021
Amirala <amiraladev@gmail.com>
2022-11-27 16:47:27 +0330

    feat(core): env package

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/infranyx/go-grpc-template/pkg/env"
+)
 
 func main() {
-	fmt.Println("InfraNyx")
+
+	url := env.New("APPLICATION_URL", []string{"foo", "jj"}).AsStringSlice(",")
+	port := env.New("APPLICATION_PORT", 4000).AsInt()
+
+	fmt.Println(url[0], port)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/infranyx/go-grpc-template
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0
