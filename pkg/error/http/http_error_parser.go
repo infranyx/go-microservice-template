9b3cbeb162cc9611e59f55adeaa8f6a48dec3740
Saman Nesari <saman@Samans-MacBook-Pro.local>
2022-12-10 22:03:57 +0330

    feat(http): add http error parser

---
+++ pkg/error/http/http_error_parser.go
@@ -0,0 +1,76 @@
+package httpError
+
+import (
+	"net/http"
+
+	errConst "github.com/infranyx/go-grpc-template/pkg/constant/error"
+	customError "github.com/infranyx/go-grpc-template/pkg/error/custom_error"
+	"google.golang.org/grpc/codes"
+)
+
+func ParseError(err error) HttpErr {
+	// check if it is http error (if it came from an external http client)
+	httpErr := AsHttpError(err)
+	if httpErr != nil {
+		return httpErr
+	}
+
+	customErr := customError.AsCustomError(err)
+	if customErr == nil {
+		err =
+			customError.NewInternalServerErrorWrap(err, errConst.ErrInfo.InternalServerErr.Msg, errConst.ErrInfo.InternalServerErr.Code, nil)
+		customErr = customError.AsCustomError(err)
+		return NewHttpError(http.StatusInternalServerError, customErr.Code(), errConst.ErrInternalServerErrorTitle, customErr.Error(), customErr.Details())
+	}
+
+	if err != nil {
+		switch {
+		case customError.IsValidationError(err):
+			return NewHttpValidationError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsBadRequestError(err):
+			return NewHttpBadRequestError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsNotFoundError(err):
+			return NewHttpNotFoundError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsInternalServerError(err):
+			return NewHttpInternalServerError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsForbiddenError(err):
+			return NewHttpForbiddenError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsUnAuthorizedError(err):
+			return NewHttpUnAuthorizedError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsDomainError(err):
+			return NewHttpDomainError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsApplicationError(err):
+			return NewHttpApplicationError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsApiError(err):
+			return NewHttpApiError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsConflictError(err):
+			return NewHttpConflictError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsUnMarshalingError(err):
+			return NewHttpInternalServerError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsMarshalingError(err):
+			return NewHttpInternalServerError(customErr.Code(), customErr.Message(), customErr.Details())
+
+		case customError.IsCustomError(err):
+			return NewHttpError(http.StatusInternalServerError, customErr.Code(), codes.Internal.String(), customErr.Message(), customErr.Details())
+
+		// case error.Is(err, context.DeadlineExceeded):
+		// 	return NewHttpError(codes.DeadlineExceeded, customErr.Code(), errorTitles.ErrRequestTimeoutTitle, err.Error(), stackTrace)
+
+		default:
+			return NewHttpInternalServerError(customErr.Code(), customErr.Message(), customErr.Details())
+		}
+	}
+
+	return nil
+}
