86316bfbbb5b0ebbc736c95b787ae9d0d2ceed7d
Saman Nesari <saman@Samans-MBP.lan>
2022-12-15 00:27:35 +0330

    fix(env): add prefix to external clients env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -67,8 +67,8 @@ func newConfig() *Config {
 			SslMode:         env.New("PG_SSL_MODE", constant.PgSslMode).AsString(),
 		},
 		GoTemplateGrpcClient: GrpcConfig{
-			Port: env.New("GO_TEMPLATE_GRPC_PORT", constant.GrpcPort).AsInt(),
-			Host: env.New("GO_TEMPLATE_GRPC_HOST", constant.GrpcHost).AsString(),
+			Port: env.New("EXTERNAL_GO_TEMPLATE_GRPC_PORT", constant.GrpcPort).AsInt(),
+			Host: env.New("EXTERNAL_GO_TEMPLATE_GRPC_HOST", constant.GrpcHost).AsString(),
 		},
 	}
 }
