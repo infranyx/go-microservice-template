1132ba3d804027bea2ddf2ac006485bc2f8dfd28
Saman Nesari <saman@Samans-MBP.lan>
2022-12-21 17:26:07 +0330

    add article dto

--- internal/article/dto/create_article_dto.go
+++ internal/article/dto/create_article_dto.go
@@ -4,6 +4,12 @@ import (
 	validator "github.com/go-ozzo/ozzo-validation"
 )
 
+type Article struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"desc"`
+}
+
 type CreateArticle struct {
 	Name        string `json:"name"`
 	Description string `json:"desc"`
