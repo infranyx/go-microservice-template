e534cb3b273467e35c92a5325eac2df921acbbb4
Saman Nesari <saman@Samans-MBP.lan>
2022-12-21 22:55:15 +0330

    feat: add article integration tests

--- go.mod
+++ go.mod
@@ -14,16 +14,20 @@ require (
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/robfig/cron/v3 v3.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
---
+++ internal/article/tests/integrations/article_test.go
@@ -0,0 +1,175 @@
+package artcileIntegrationTest
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/infranyx/go-grpc-template/app"
+	articleDto "github.com/infranyx/go-grpc-template/internal/article/dto"
+	"github.com/infranyx/go-grpc-template/pkg/config"
+	grpcError "github.com/infranyx/go-grpc-template/pkg/error/grpc"
+	httpError "github.com/infranyx/go-grpc-template/pkg/error/http"
+	"github.com/infranyx/go-grpc-template/pkg/grpc"
+	httpClient "github.com/infranyx/go-grpc-template/pkg/http/client"
+	articleV1 "github.com/infranyx/protobuf-template-go/golang-grpc-template/article/v1"
+	"github.com/labstack/echo/v4"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc/codes"
+)
+
+type ArticleSuiteTests struct {
+	suite.Suite
+	articleClient articleV1.ArticleServiceClient
+}
+
+func (suite *ArticleSuiteTests) SetupSuite() {
+	go app.New().Run()
+	time.Sleep(5 * time.Second)
+
+	articleClient, err := grpc.NewGrpcClient(&grpc.GrpcConfig{
+		Port: config.Conf.Grpc.Port,
+		Host: config.Conf.Grpc.Host,
+	})
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
+
+	suite.articleClient = articleV1.NewArticleServiceClient(articleClient.GetGrpcConnection())
+}
+
+func (suite *ArticleSuiteTests) TearDownSuite() {
+	// suite.cleanup()
+}
+
+func (suite *ArticleSuiteTests) TestSuccessCreateGrpcArticle() {
+	ctx := context.Background()
+	input := &articleV1.CreateArticleRequest{
+		Name: "John",
+		Desc: "Pro Developer",
+	}
+	res, err := suite.articleClient.CreateArticle(ctx, input)
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
+
+	assert.NotNil(suite.T(), res.Id)
+	assert.Equal(suite.T(), "John", res.Name)
+	assert.Equal(suite.T(), "Pro Developer", res.Desc)
+}
+
+func (suite *ArticleSuiteTests) TestNameValidationErrCreateGrpcArticle() {
+	ctx := context.Background()
+	input := &articleV1.CreateArticleRequest{
+		Name: "Jo",
+		Desc: "Pro Developer",
+	}
+	_, err := suite.articleClient.CreateArticle(ctx, input)
+
+	assert.NotNil(suite.T(), err)
+	grpcErr := grpcError.ParseExternalGrpcErr(err)
+	assert.NotNil(suite.T(), grpcErr)
+	assert.Equal(suite.T(), codes.InvalidArgument, grpcErr.GetStatus())
+	assert.Contains(suite.T(), grpcErr.GetDetails(), "name")
+}
+
+func (suite *ArticleSuiteTests) TestDescValidationErrCreateGrpcArticle() {
+	ctx := context.Background()
+	input := &articleV1.CreateArticleRequest{
+		Name: "John",
+		Desc: "Pro",
+	}
+	_, err := suite.articleClient.CreateArticle(ctx, input)
+
+	assert.NotNil(suite.T(), err)
+
+	grpcErr := grpcError.ParseExternalGrpcErr(err)
+	assert.NotNil(suite.T(), grpcErr)
+	assert.Equal(suite.T(), codes.InvalidArgument, grpcErr.GetStatus())
+	assert.Contains(suite.T(), grpcErr.GetDetails(), "desc")
+}
+
+func (suite *ArticleSuiteTests) TestSuccessCreateHttpArticle() {
+	ctx := context.Background()
+	articleJSON := `{"name":"John Snow","desc":"King of the north"}`
+
+	res, err := httpClient.
+		BuildReq().
+		SetContext(ctx).
+		SetBody(strings.NewReader(articleJSON)).
+		SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
+		Post(fmt.Sprintf("http://localhost:%d%s/article", config.Conf.Http.Port, "/api/v1")).
+		Execute()
+
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
+
+	articleDto := new(articleDto.Article)
+	err = res.Bind(articleDto)
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
+
+	assert.NotNil(suite.T(), articleDto.ID)
+	assert.Equal(suite.T(), "John Snow", articleDto.Name)
+	assert.Equal(suite.T(), "King of the north", articleDto.Description)
+}
+
+func (suite *ArticleSuiteTests) TestNameValidationErrCreateHttpArticle() {
+	ctx := context.Background()
+	articleJSON := `{"name":"Jo","desc":"King of the north"}`
+
+	res, err := httpClient.
+		BuildReq().
+		SetContext(ctx).
+		SetBody(strings.NewReader(articleJSON)).
+		SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
+		Post(fmt.Sprintf("http://localhost:%d%s/article", config.Conf.Http.Port, "/api/v1")).
+		Execute()
+
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
+
+	httpErr := httpError.ParseExternalHttpErr(res.Body())
+
+	assert.NotNil(suite.T(), httpErr)
+	assert.Equal(suite.T(), http.StatusBadRequest, httpErr.GetStatus())
+	assert.Equal(suite.T(), false, res.IsSuccess())
+	assert.Contains(suite.T(), httpErr.GetDetails(), "name")
+}
+
+func (suite *ArticleSuiteTests) TestDescValidationErrCreateHttpArticle() {
+	ctx := context.Background()
+	articleJSON := `{"name":"John Snow","desc":"King"}`
+
+	res, err := httpClient.
+		BuildReq().
+		SetContext(ctx).
+		SetBody(strings.NewReader(articleJSON)).
+		SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
+		Post(fmt.Sprintf("http://localhost:%d%s/article", config.Conf.Http.Port, "/api/v1")).
+		Execute()
+
+	if err != nil {
+		assert.Error(suite.T(), err)
+	}
+
+	httpErr := httpError.ParseExternalHttpErr(res.Body())
+
+	assert.NotNil(suite.T(), httpErr)
+	assert.Equal(suite.T(), http.StatusBadRequest, httpErr.GetStatus())
+	assert.Equal(suite.T(), false, res.IsSuccess())
+	assert.Contains(suite.T(), httpErr.GetDetails(), "desc")
+}
+
+func TestRunSuite(t *testing.T) {
+	suiteTester := new(ArticleSuiteTests)
+	suite.Run(t, suiteTester)
+}
